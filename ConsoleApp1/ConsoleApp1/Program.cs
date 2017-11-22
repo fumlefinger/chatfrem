573467c05ac975e3430cf45ca1aa7c3de979fde9
Fumlefinger <jann2273@gmail.com>
2017-11-22 15:50:14 +0100

    bærbar

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static int roomId;
+        static string userInput;
+        static List<Rooms> rooms = new List<Rooms>();
+        
+
+
+        static void Main(string[] args)
+        {
+            rooms.Add(new Rooms("c-sharp room"));
+            rooms.Add(new Rooms("python room"));
+            ListRooms();
+            JoinRoom();
+            
+
+        
+           
+        }
+
+        static void ListRooms()
+        {
+            
+            for (int i = 0; i < rooms.Count();i++)
+            {
+                Console.WriteLine("["+i+"] -  "+rooms[i].returnName());
+
+            }
+            Console.WriteLine("Please enter the number of the room you would like to enter: ");
+            userInput = Console.ReadLine();
+        }
+
+        static void JoinRoom()
+        {
+            for (int i = 0; i < rooms.Count();i++)
+            {
+                if (int.Parse(userInput) == i)
+                {
+                    rooms[i].PrintLog();
+                    roomId = i;
+                    break;
+
+                }
+            }
+            Chatting(roomId);
+        }
+
+        static void Chatting(int roomId)
+        {
+            while(true)
+            {
+                userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "exit")
+                {
+                    break;
+                }
+                else if (userInput.ToLower() == "logout")
+                {
+                    break;
+                }
+                else
+                {
+                    rooms[roomId].SaveLog(userInput);
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Rooms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rooms
+    {
+
+
+        List<string> chatLog = new List<string>();
+        string roomName;
+
+        public Rooms(string name)
+        {
+
+            this.roomName = name;
+
+        }
+
+        public string returnLog(int number)
+        {
+            return this.chatLog[number];
+        }
+
+        public string returnName()
+        {
+            return this.roomName;
+        }
+
+        public void PrintLog()
+        {
+            Console.Clear();
+            for (int i = 0; i < this.chatLog.Count(); i++)
+            {
+                Console.WriteLine(this.chatLog[i]);
+                Console.WriteLine();
+            }
+        }
+
+        public void SaveLog(string text)
+        {
+            this.chatLog.Add(text);
+        }
+
+    }
+}
